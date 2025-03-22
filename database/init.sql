2bd7ce30ca92faad9c4d203cc0bad10ee26ee394
lleenBarri <leen.barri.1@gmail.com>
2025-03-22 19:34:25 +0200

    Initial commit for urgentNews project

---
+++ database/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE News (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
+);
+
+INSERT INTO News (title, content) VALUES
+('Urgent News 1', 'Content urgent news 1'),
+('Urgent News 2', 'Content urgent news 2'),
+('Urgent News 3', 'Content urgent news 3'),
+('Urgent News 4', 'Content urgent news 4'),
+('Urgent News 5', 'Content urgent news 5'),
+('Urgent News 6', 'Content urgent news 6'),
+('Urgent News 7', 'Content urgent news 7'),
+('Urgent News 8', 'Content urgent news 8'),
+('Urgent News 9', 'Content urgent news 9'),
+('Urgent News 10', 'Content urgent news 10');
